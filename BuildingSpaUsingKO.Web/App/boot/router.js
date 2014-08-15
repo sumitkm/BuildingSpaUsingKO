d717f740f0a3002069c267afaa800b6246f9778e
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-08-15 22:31:48 +0100

    Updated "Settings" route

--- BuildingSpaUsingKO.Web/App/boot/router.js
+++ BuildingSpaUsingKO.Web/App/boot/router.js
@@ -4,21 +4,9 @@ define(["jquery", "knockout", "crossroads", "historyjs"], function ($, ko, cross
     return new Router({
         routes: [
             { url: '/', params: { page: 'home' } },
-            { url: '/settings', params: { page: 'settings' } }
+            { url: 'settings', params: { page: 'settings' } }
         ]
     });
-
-    //function change(state) {
-    //    if (state === null) {
-    //        crossroads.parse('/');
-    //    } else {
-    //        crossroads.parse(state.url);
-    //    }
-    //}
-
-    //$(window).bind("popstate", function (e) {
-    //    change(e.originalEvent.state);
-    //});
 
     function Router(config) {
         var currentRoute = this.currentRoute = ko.observable({});
@@ -28,7 +16,6 @@ define(["jquery", "knockout", "crossroads", "historyjs"], function ($, ko, cross
                 currentRoute(ko.utils.extend(requestParams, route.params));
             });
         });
-        //crossroads.routed.add(console.log, console);
         activateCrossroads();
         $("body").on("click", "a",
             function (e) {
@@ -45,37 +32,8 @@ define(["jquery", "knockout", "crossroads", "historyjs"], function ($, ko, cross
             });
     }
 
-
-
     function activateCrossroads() {
-        //function parseHash(newHash, oldHash)
-        //{
-        //    //if (newHash == "")
-        //    //{
-        //    //    if(location.pathname != "/" && location.pathname.length > 1)
-        //    //    {
-        //    //        newHash = location.pathname.slice(1);
-        //    //    }
-        //    //}
-        //    crossroads.parse(newHash);
-        //}
-
-        //function changeHash(newHash, oldHash) {
-        //    var route = newHash;
-        //    //if (route) {
-        //    //    history.replaceState({url: route}, "", location.protocol + '//' + location.host + '/' + route);
-        //    //}
-        //    //else {
-        //    //    history.replaceState({url: '/' }, "", location.protocol + '//' + location.host);
-        //    //}
-        //    crossroads.parse(newHash);
-        //}
         crossroads.normalizeFn = crossroads.NORM_AS_OBJECT;
-
-        //hasher.initialized.add(parseHash);
-        //hasher.changed.add(changeHash);
-        //hasher.init();
-
         crossroads.parse('/');
 
         History = window.History;
