21f063b26143ac2f5474f9323b5aff1fc97e6cf4
Sumit <sumitkm@gmail.com>
2014-09-25 21:05:12 +0100

    Part 6: Custom Components - Tab

--- BuildingSpaUsingKO.Web/App/components/tabbed-navigation/tabbed-navigation.js
+++ BuildingSpaUsingKO.Web/App/components/tabbed-navigation/tabbed-navigation.js
@@ -1,36 +1,43 @@
 ﻿define(["knockout", "underscore", "text!./tabbed-navigation.html"], function (ko, _, tabbedNavigationTemplate) {
     var isInitialized = false;
     var tabsCache = [];
+    var selectedIndexCache = 0;
     function tabbedNavigationViewModel(params) {
         self = this;
+        if (!isInitialized) {
+            isInitialized = true;
+            tabsCache = params.tabConfig().tabs;
+        }
         self.tabs = ko.observableArray(tabsCache);
-        self.selectedIndex = ko.observable(0);
+        self.selectedIndex = ko.observable(selectedIndexCache);
         self.selected = ko.pureComputed(function () {
             return self.tabs()[self.selectedIndex()];
         }, this);
 
-        if (!isInitialized) {
-            isInitialized = true;
-            tabsCache = params.tabConfig().tabs;
-            self.tabs = ko.observableArray(tabsCache);
-        }
+
 
         if (params.route) {
-            selectByName(params.route);
+            selectByRoute(params.route);
         }
 
-        function selectByName(params) {
+        function selectByRoute(params) {
             var item = _.find(self.tabs(), function (tab) {
                 return tab.url == params();
             });
             if (item) {
-                _.each(self.tabs(), deselect);
+                self.tabs()[self.selectedIndex()].isSelected = false;
                 var index = self.tabs().indexOf(item);
-                self.tabs()[index].isSelected = true;
-                self.selectedIndex(index);
+                select(index);
             }
             return item;
         };
+
+        function select(index)
+        {
+            selectedIndexCache = index;
+            self.tabs()[index].isSelected = true;
+            self.selectedIndex(index);
+        }
 
         function deselect(tab) {
             tab.isSelected = false;
