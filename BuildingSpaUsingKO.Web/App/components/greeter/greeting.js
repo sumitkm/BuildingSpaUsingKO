b445db426059b49c4b24a06513cc6c0364516baa
Sumit <sumitkm@gmail.com>
2015-07-12 13:09:03 +0100

    Added dispose method to dispose Handlers correctly

--- BuildingSpaUsingKO.Web/App/components/greeter/greeting.js
+++ BuildingSpaUsingKO.Web/App/components/greeter/greeting.js
@@ -4,10 +4,16 @@
         var self = this;
         self.greeting = ko.observable(params.name);
         self.date = ko.observable(new Date());
-        amplify.subscribe("CurrentNode-NodeClicked", function (value) {
-            self.greeting(value);
-        });
+        amplify.subscribe("CurrentNode-NodeClicked", this, setGreeting);
         return self;
+    }
+
+    function setGreeting(value) {
+        this.greeting(value);
+    }
+
+    function dispose() {
+        amplify.unsubscribe("CurrentNode-NodeClicked", setGreeting);
     }
     return { viewModel: greeterViewModel, template: greeterTemplate };
 });
